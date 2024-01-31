bd64958bf209640e21e815c3112f165aeda9036c
kabuzo13 <beahonigmann@gmail.com>
2024-01-31 19:27:57 +0000

    displaying title and image of search

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
-APIkey = "e7240985d24e036814dfc3709dd38d80"
-queryURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=6530dc18&app_key=e7240985d24e036814dfc3709dd38d80"
+APIkey = "e7240985d24e036814dfc3709dd38d80";
 
 
 function getRecipe() {
+    var search = $('#search-input').val().trim();
+    queryURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" + search + "&app_id=6530dc18&app_key=e7240985d24e036814dfc3709dd38d80";
+
     fetch(queryURL)
         .then(function (response) {
             return response.json();   
         })
         .then(function (data) {
             console.log(data);
+            $('.card-title').text(data.hits[0].recipe.label);
+            $('img').attr('src', data.hits[0].recipe.image);
         });
 
-    var search = $('#search-input').val().trim();
     console.log(search);
     return search;
 }
 
-$('#search-button').on('click', getRecipe())
+$('#search-button').on('click', function(event) {
+    event.preventDefault();
+    var search = $('#search-input').val().trim();
+    if (!search) return;
+    getRecipe();
+})
