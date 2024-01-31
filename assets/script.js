d58f263ed65ddd656f32192fb76534005da78f0a
sohail <as19adj@herts.ac.uk>
2024-01-31 19:42:50 +0000

    Please ignore this commit and deny it

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,37 @@
-APIkey = "e7240985d24e036814dfc3709dd38d80"
-queryURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=6530dc18&app_key=e7240985d24e036814dfc3709dd38d80"
+var apiKey = "e7240985d24e036814dfc3709dd38d80"
+var apiURL = "https://api.edamam.com/api/recipes/v2?type=public&"
+var app_id = "6530dc18"
+var queryURL;
 
+console.log("Hey");
 
-function getRecipe() {
+// function getRecipe() {
+//     fetch(queryURL)
+//         .then(function (response) {
+//             return response.json();   
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+
+//     var search = $('#search-input').val().trim();
+//     console.log(search);
+//     return search;
+// }
+
+// $('#search-button').on('click', getRecipe())
+
+$("#search-button").on("click", function(){
+
+    console.log("Hey");
+    var search = $('#search-button').val().trim();
+    queryURL = apiURL + "q=" + search + "&" + app_id + "&" + apiKey;
+    console.log("Query: ", queryURL);
     fetch(queryURL)
-        .then(function (response) {
-            return response.json();   
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-
-    var search = $('#search-input').val().trim();
-    console.log(search);
-    return search;
-}
-
-$('#search-button').on('click', getRecipe())
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+                // console.log(data.articles);
+});
+});
